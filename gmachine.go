70d29941bace0e5be565accdae76db863e55ad4f
richard bright <rkbright1@gmail.com>
2020-07-30 05:53:22 -0700

    test 1 passed

--- gmachine.go
+++ gmachine.go
@@ -5,15 +5,18 @@ package gmachine
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
 
-type Mem struct {
+type Machine struct {
 	P      uint64
 	Memory []uint64
 }
 
-func New() *Mem {
-	return &Mem{}
+func New() *Machine {
+	m := Machine{}
+	m.Memory = make([]uint64, 1024)
+	return &Machine{Memory: m.Memory}
 }
 
-func (m *Mem) GetMemory(in uint64) uint64 {
-	return 0
-}
+// func (m *Machine) GetMemory(register uint64, memory []uint64) uint64 {
+
+// 	return 0
+// }
